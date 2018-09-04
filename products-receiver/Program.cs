26e43fcd7b2f4335fe16837ef14ace6f635c8c64
Jason Agostoni <jagostoni@ceiamerica.com>
2018-09-04 19:12:56 -0400

    Added console receiver for products topic

---
+++ products-receiver/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using Newtonsoft.Json;
+
+namespace products_receiver
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            var host = Environment.GetEnvironmentVariable("ACTIVE_MQ_HOST") ?? "localhost";
+            Console.WriteLine($"ActiveMQ Host: {host}");
+
+                var factory = new ConnectionFactory(new Uri($"tcp://{host}:61616"));
+                var connection = factory.CreateConnection();
+                var session = connection.CreateSession();
+                var target = session.GetTopic("product-updates");
+                var consumer = session.CreateConsumer(target, null);
+                consumer.Listener += message => {
+                    var productJson = (message as ITextMessage)?.Text;
+                    if(!String.IsNullOrEmpty(productJson)) {
+                        try {
+                            var product = JsonConvert.DeserializeObject<Product>(productJson);
+                            Console.WriteLine($"Received Product ID: {product.Id}");
+                        } catch {}
+                    }
+                };
+                connection.Start();
+            
+
+            Console.WriteLine("Press enter to close");
+            Console.ReadLine();
+    
+        }
+    }
+}
