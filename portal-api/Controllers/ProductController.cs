8c8de9ff58e874372a9f1532dde3e143b25d8ad8
Jason Agostoni <JAgostoni@ceiamerica.com>
2018-09-06 16:16:10 -0400

    Proper JSON returned from portal search

--- portal-api/Controllers/ProductController.cs
+++ portal-api/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using StackExchange.Redis;
 
 namespace portal_api.Controllers
@@ -32,11 +33,13 @@ namespace portal_api.Controllers
 
         
         [HttpGet("find/{search}")]
-        public IEnumerable<string> Find(string search)
+        public IEnumerable<Product> Find(string search)
         {
             var server = _REDIS.GetServer(_REDIS.GetEndPoints()[0]);
             IDatabase redisDB = _REDIS.GetDatabase();
-            return server.Keys(pattern:$"*{search}*").Select(k=>redisDB.StringGet(k).ToString());
+            return server.Keys(pattern:$"*{search}*")
+                        .Select(k=>redisDB.StringGet(k).ToString())
+                        .Select(pjson => JsonConvert.DeserializeObject<Product>(pjson));
         }
     }
 }
