75fb4862d66af9db46b795e29220c297b7d8760d
Jason Agostoni <jagostoni@ceiamerica.com>
2018-09-05 20:39:17 -0400

    Added test data generator client

---
+++ test-data-gen/Product.cs
@@ -0,0 +1,7 @@
+using System;
+
+public class Product {
+    public string Slug { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
---
+++ test-data-gen/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace test_data_gen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prodGen = new Bogus.Faker<Product>()
+                .RuleFor(p=>p.Name, f=>f.Commerce.ProductName())
+                .RuleFor(p=>p.Slug, (f,p)=>p.Name.Replace(" ", "-").ToLower())
+                .RuleFor(p=>p.Description, f=>f.Lorem.Paragraph());
+
+            HttpClient c = new HttpClient();
+
+            prodGen.Generate(3000).ForEach(p=>
+            {
+                var productJson = JsonConvert.SerializeObject(p);
+                var res = c.PostAsync("http://localhost:5002/api/products", 
+                                      new StringContent(productJson, Encoding.UTF8, "application/json")).Result;
+                Console.WriteLine(res.StatusCode);
+                Console.WriteLine($"{p.Slug} : {p.Name}");
+            });
+
+        }
+    }
+}
