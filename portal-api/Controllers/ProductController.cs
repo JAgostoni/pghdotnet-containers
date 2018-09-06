d4c72650a84d5ec0037012a65e498c175b474b94
Jason Agostoni <jagostoni@ceiamerica.com>
2018-09-05 20:05:09 -0400

    Added portal / seach API

---
+++ portal-api/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace portal_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        ConnectionMultiplexer _REDIS;
+
+        public ProductController()
+        {
+            var redisHost = Environment.GetEnvironmentVariable("REDIS_HOST") ?? "localhost";
+            Console.WriteLine($"Redis Host: {redisHost}");
+
+            _REDIS = ConnectionMultiplexer.Connect(redisHost);
+
+        }
+        
+        // GET api/values/5
+        [HttpGet("{slug}")]
+        public async Task<string> Get(string slug)
+        {
+            IDatabase redisDB = _REDIS.GetDatabase();
+            return (await redisDB.StringGetAsync(slug)).ToString();
+        }
+
+        
+        [HttpGet("find/{search}")]
+        public IEnumerable<string> Find(string search)
+        {
+            var server = _REDIS.GetServer(_REDIS.GetEndPoints()[0]);
+            IDatabase redisDB = _REDIS.GetDatabase();
+            return server.Keys(pattern:$"*{search}*").Select(k=>redisDB.StringGet(k).ToString());
+        }
+    }
+}
